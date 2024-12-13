8433b7391c69377dc95051084b92d42ac10f61ff
Laszlo E Kovacs <laszloekovacs@gmail.com>
2024-12-13 17:53:18 +0100

    updated schema

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,6 +1,6 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
+import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users', {
-	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	id: uuid().primaryKey().defaultRandom(),
 	email: varchar('email', { length: 255 }).notNull().unique()
 })
